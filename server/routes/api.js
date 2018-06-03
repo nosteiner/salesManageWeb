836b9ea32ff19f80f1762d8637ac8a8717baef1e
Omi <omer257@gmail.com>
2018-06-03 16:28:16 +0300

    Update api.js
    Updates dogs array on the server. (copied from the service)

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const DOGS = [
-    {id: 1, name: 'Rex', weight: 20, birthDate: new Date(2006, 2, 21)},
-    {id: 2, name: 'Woof', weight: 8, birthDate: new Date(2011, 8, 12)},
-    {id: 3, name: 'Chuck', weight: 28, birthDate: new Date(2015, 5, 6)},
-    {id: 4, name: 'Barkley', weight: 4, birthDate: new Date(2012, 3, 15)},
-    {id: 5, name: 'Prince', weight: 65, birthDate: new Date(2017, 5, 4)}
+  {id: 0, name: 'Rex', weight: 20, birthDate: new Date(2006, 2, 21), owner: 'Jack Daniels', walks: []},
+  {id: 1, name: 'Woof', weight: 8, birthDate: new Date(2011, 8, 12), owner: 'Mike Perry', walks: []},
+  {id: 2, name: 'Chuck', weight: 28, birthDate: new Date(2015, 5, 6), owner: 'Sarah Abrahamson', walks: []},
+  {id: 3, name: 'Barkley', weight: 4, birthDate: new Date(2012, 3, 15), owner: 'Lara Croft', walks: []},
+  {id: 4, name: 'Prince', weight: 65, birthDate: new Date(2017, 5, 4), owner: 'Jerry Seinfeld', walks: []}
 ]
 
 /* GET api listing. */
